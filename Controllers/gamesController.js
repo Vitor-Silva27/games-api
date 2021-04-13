a156c8d699f9399b75311e80b5bac52aa5e737ae
Vitor Silva <developer.vitorsilva@gmail.com>
2021-04-13 20:25:42 -0300

    created delete method

--- Controllers/gamesController.js
+++ Controllers/gamesController.js
@@ -1,56 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const Game = require("../Models/Game");
 
-router.get("/games",(_, res) =>{
-    Game.findAll().then(game =>{
-            res.json(game);
+router.get("/games", (_, res) => {
+  Game.findAll().then((game) => {
+    res.json(game);
+  });
+  res.statusCode = 200;
+});
+
+router.get("/game/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.sendStatus(400);
+  } else {
+    Game.findOne({
+      where: {
+        id: id,
+      },
     })
-    res.statusCode = 200;
+      .then((game) => {
+        res.json(game);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
 
-router.get("/game/:id", (req, res) =>{
-    const id = parseInt(req.params.id);
-    if(isNaN(id)){
-        res.sendStatus(400);
-    }else{
-        Game.findOne({
-            where:{
-                id: id
-            }
-        }).then(game => {
-            res.json(game);
-        }).catch(err =>{
-            console.log(err);
-        })
-    }
-}); 
-
-router.post("/game", (req, res) =>{
-    const { title, price, year } = req.body;
-    Game.create({
-        title: title,
-        price: price,
-        year: year
-    });
-    res.sendStatus(200);
-    
+router.post("/game", (req, res) => {
+  const { title, price, year } = req.body;
+  Game.create({
+    title: title,
+    price: price,
+    year: year,
+  });
+  res.sendStatus(200);
 });
-/* 
-router.delete("/game/:id", (req, res) =>{
-    const id = parseInt(req.params.id);
-    if(isNaN(id)){
-        res.sendStatus(400);
-    }else{
-        const index = DB.games.findIndex(game => game.id === id);
-        
-        if(index == -1){
-            res.sendStatus(400);
-        }else{
-            DB.games.splice(index, 1);
-            res.sendStatus(200);
-        }
-    }
-}); */
+
+router.delete("/game/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.sendStatus(400);
+  } else {
+    Game.destroy({
+      where: {
+        id: id,
+      },
+    }).then(() =>{
+        res.sendStatus(200);
+    })
+  }
+});
 
 module.exports = router;
